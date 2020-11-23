fac3354bf311d3989db30d72860efb91dc852ecb
Tgoad22 <73500527+Tgoad22@users.noreply.github.com>
2020-11-23 09:25:49 -0600

    Add project files.

---
+++ WorkingWithFiles/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace WorkingWithFiles
+{
+
+    public static class WorkingWithFiles
+    {
+        // 1- Write a method that reads a text file and returns the number of words. Ex. The file
+        // contains, "The dog ran away from home." The method should return "6" the number of words
+        // contained in the file.
+        public static int WordCount()
+        {
+            // Delete the below line when you are writing your code 
+            return default;
+        }
+
+        // 2- Write a method that reads a text file and returns the longest word in the file. Ex.
+        // The file contains, "The dog named Maximus ran away from home." The method should return
+        // "Maximus" the longest word contained in the file.
+        public static string LongestWord()
+        {
+            // Delete the below line when you are writing your code 
+            return default;
+        }
+    }
+
+    public static class Program
+    {
+        public static void Main()
+        {
+            // Use this section to test code should you feel the need.
+            //Console.WriteLine(YOUR CODE HERE);
+        }
+    }
+}
