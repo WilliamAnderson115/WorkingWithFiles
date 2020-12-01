1027e9761355bcd71cfeeb35014d2ea6ae7cd433
Tgoad22 <73500527+Tgoad22@users.noreply.github.com>
2020-12-01 12:47:32 -0600

    Corrected possible misunderstanding in prompt

--- WorkingWithFiles/Program.cs
+++ WorkingWithFiles/Program.cs
@@ -14,7 +14,7 @@ namespace WorkingWithFiles
         // 1- Write a method that reads a text file and returns the number of words. Ex. The file
         // contains, "The dog ran away from home." The method should return "6" the number of words
         // contained in the file. Return "0" for the word count if the file is empty.
-        // Do not forget to Trim every file.
+        // .Trim() might be useful in this situation.
         public static int WordCount(string fileName)
         {
             return default;
@@ -23,7 +23,7 @@ namespace WorkingWithFiles
         // 2- Write a method that reads a text file and returns the longest word in the file. Ex.
         // The file contains, "The dog named Maximus ran away from home." The method should return
         // "Maximus" the longest word contained in the file. Return "File is empty" if the file is empty.
-        // Do not Trim every file.
+        // .Trim() might be useful in this situation.
         public static string LongestWord(string fileName)
         {
             return default;
